8b316efd929e84ff5053ba490be4fff231e13ddf
ackushiw <ackw.kushi@gmail.com>
2020-01-01 21:13:53 -0500

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  // env: {
+  //   browser: true,
+  //   node: true
+  // },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  extends: ['@nuxtjs'],
+  // add your custom rules here
+  rules: {}
+}
